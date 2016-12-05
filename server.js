6d29f53ae205e19e9c92a05428412a0ffd5e7391
CallistoCF <chrisfrater@gmail.com>
2016-12-04 20:51:09 -0800

    Changed to related artist

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
+async = require("async");
 
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
@@ -29,27 +30,56 @@ app.get('/search/:name', function(req, res) {
 
     searchReq.on('end', function(item) {
         //console.log("Artist is " + artist.name + " id is " + item.artists.items[0].id);
-        var a = getRelated(item.artists.items[0].id);
-        var artist = a;
-        res.json(a);
+        //var a = getRelated(item.artists.items[0].id);
+        var artist = item.artists.items[0];
+        var artistn = item.artists.items[0].name;
+        var artistid = item.artists.items[0].id;
+        console.log("artist is " + artistn + " id is " + artistid);
+        //res.json(artist);
+        var s = 'https://api.spotify.com/v1/artists/' + artistid + '/related-artists';
+        console.log("s is " + s);
+        unirest.get(s)
+               .end(function(response) {
+                    if (response.ok) {
+                        for (var i = 0; i < 10; i++) {
+                          console.log(i + ") " + response.body.artists[i].name + " " + response.body.artists[i].id);
+                        }
+                        res.json(response.body.artists[0]);
+                    }
+                    else {
+                        console.log("error " + response.code);
+                    }
+              });
     });
 
     searchReq.on('error', function(code) {
         res.sendStatus(code);
     });
+
 });
 
+/*
+app.get('/search/:name', function(req, res) {
+  console.log("id get called");
+  var searcho = getFromApi('artists', {
+    q: req.params.name,
+    type: 'related-artists'
+  });
+
+  searcho.on('end', function(item) {
+    console.log("Searcho: end");
+    var artists = item.artists;
+    res.json(artists);
+  });
+
+  searcho.on('error', function(code) {
+    console.log("Searcho: error");
+    console.log(code);
+    res.sendStatus(code);
+  });
+});
+*/
 function getRelated(id) {
-  console.log("Get related called with " + id);
-  function rsearch(id) {
-    unirest.get('https://api.spotify.com/v1/artists/' + id + '/related-artists')
-    .end(function(response){
-      console.log(response.body.artists);
-      return response.body.artists;
-    });
-  }
-  rsearch(id);
-  //console.log(Request);
-  console.log("Get related artists complete");
+
 }
 app.listen(process.env.PORT || 8080);
